15d6c83139e31ee96fae0529271e717e448e012e
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-22 11:35:53 +0300

    Initial Commit

---
+++ FlightTimeline/FlightTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimelineApp.swift
+//  FlightTimeline
+//
+//  Created by MikhaiL on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
