028af6e99af1a2c89a9dbcf38e5a21324df57983
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-23 19:32:23 +0300

    сlick through cells

--- FlightTimeline/FlightRowView.swift
+++ FlightTimeline/FlightRowView.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 
 struct FlightRowView: View {
+    @State private var isPresented = false
+    
     let flight: FlightInformation
     
     var body: some View {
-        HStack {
-            Text("\(flight.airline) \(flight.number)")
-                .frame(width: 120, alignment: .leading)
-            Text(flight.otherAirport)
-                .frame(alignment: .leading)
-            Spacer()
-            
-            Text(flight.flightStatus)
+        Button(action: { isPresented.toggle() } ) {
+            HStack {
+                Text("\(flight.airline) \(flight.number)")
+                    .frame(width: 120, alignment: .leading)
+                Text(flight.otherAirport)
+                    .frame(alignment: .leading)
+                Spacer()
+                
+                Text(flight.flightStatus)
+            }
+        }
+        .sheet(isPresented: $isPresented) {
+            FlightDetailsView(isPresented: $isPresented, flight: flight)
         }
     }
 }
